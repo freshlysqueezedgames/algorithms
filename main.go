55bec174baa7179bce2139c4ad31f8c13ad60d38
Carl Pryke <carl.p@freshly-squeezed.com>
2022-07-31 20:33:25 +0100

    selection sort

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-func main() {
-	sorting.SelectionSort([]int{}, func(a int, b int) {
+import (
+	"log"
 
-	})
+	"github.com/freshlysqueezedgames/algorithms/sorting"
+)
+
+func comparitor(a int, b int) int {
+	if a < b {
+		return -1
+	}
+
+	if a > b {
+		return 1
+	}
+
+	return 0
+}
+
+func main() {
+	log.Printf("sorted: %v", sorting.SelectionSort([]int{64, 25, 15, 22, 11}, comparitor))
 }
--- sorting/sorting.go
+++ sorting/sorting.go
@@ -1,7 +1,37 @@
 package sorting
 
-type Comparitor[K int] func(a K, b K) int
+type Comparable interface {
+	string | int | float32 | float64 | int32 | int64 | uint
+}
 
-func SelectionSort[K int](list []K, comparitor Comparitor[K]) []K {
-	return []K{}
+// returns -1 for move a is less than b, 0 for equality / do nothing, 1 for a is more than b
+type Comparitor[K Comparable] func(a K, b K) int
+
+func SelectionSort[K Comparable](list []K, comparitor Comparitor[K]) {
+	length := len(list)
+
+	if length == 0 {
+		return
+	}
+
+	lowestIndex := 0
+
+	for i := 0; i < length; i++ {
+		sorted := true
+
+		for j := i; j < length; j++ {
+			if comparitor(list[j], list[lowestIndex]) == -1 {
+				sorted = false
+				lowestIndex = j
+			}
+		}
+
+		if sorted {
+			return
+		}
+
+		temp := list[lowestIndex]
+		list[lowestIndex] = list[i]
+		list[i] = temp
+	}
 }
